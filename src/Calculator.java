88c2f1d2be277bb0381ab86029e18c2ef2f1ade7
Sowmya Narayanan <sowmya_narayanan@outlook.com>
2017-06-22 22:51:09 +0530

    Implemented addition

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+	public static void main(String[] args) {
+		int r = Add(4, 2);
+		System.out.println(r);
+
+	}
+
+	public static int Add(int num1, int num2) {
+		int result = num1 + num2;
+		return result;
+	}
+}
