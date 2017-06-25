579923db6a84c8bd3b1d739f3a8f9d936a988fb1
Sowmya Narayanan <sowmya_narayanan@outlook.com>
2017-06-25 23:37:30 +0530

    implemented subtraction, multiplication and division.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,30 @@
 
 public class Calculator {
 	public static void main(String[] args) {
-		int r = Add(4, 2);
-		System.out.println(r);
-
+		int ra = Add(4, 2);
+		int rs = Sub(4, 2);
+		int rm = Multiply(4, 2);
+		int rd = Div(4, 2);
+		System.out.println(ra + "\n" + rs + "\n" + rm + "\n" + rd);
 	}
 
 	public static int Add(int num1, int num2) {
 		int result = num1 + num2;
+		return result;
+	}
+
+	public static int Sub(int num1, int num2) {
+		int result = num1 - num2;
+		return result;
+	}
+
+	public static int Multiply(int num1, int num2) {
+		int result = num1 * num2;
+		return result;
+	}
+
+	public static int Div(int num1, int num2) {
+		int result = num1 / num2;
 		return result;
 	}
 }
