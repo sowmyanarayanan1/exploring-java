292bb468184b9bc3885fc04776c5da9258825c4b
Sowmya Narayanan <sowmya_narayanan@outlook.com>
2017-07-02 21:55:15 +0530

    fixed average issue and data type in calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,31 +1,31 @@
 
 public class Calculator {
 	public static void main(String[] args) {
-		Calculator calculator = new Calculator();
+		//Calculator calculator = new Calculator();
 
-		int additionResult = calculator.add(4, 2);
-		int subtractionResult = calculator.subtract(4, 2);
-		int multiplicationResult = calculator.multiply(4, 2);
-		double divisionResult = calculator.divide(3, 2);
+		//int additionResult = calculator.add(4, 2);
+		//int subtractionResult = calculator.subtract(4, 2);
+		//int multiplicationResult = calculator.multiply(4, 2);
+		//double divisionResult = calculator.divide(3, 2);
 		
-		System.out.println(additionResult + "\n" + subtractionResult + "\n" + multiplicationResult + "\n" + divisionResult);
+		//System.out.println(additionResult + "\n" + subtractionResult + "\n" + multiplicationResult + "\n" + divisionResult);
 		
-		int elements[] = { 2, 4, 6, 8, 9, 12 };
-		System.out.println(calculator.calculateAverage(elements));
+		//int elements[] = { 2, 4, 6, 8, 9, 12 };
+		//System.out.println(calculator.calculateAverage(elements));
 	}
 
-	public int add(int num1, int num2) {
-		int result = num1 + num2;
+	public double add(double num1, int num2) {
+		double result = num1 + num2;
 		return result;
 	}
 
-	public int subtract(int num1, int num2) {
-		int result = num1 - num2;
+	public double subtract(double num1, int num2) {
+		double result = num1 - num2;
 		return result;
 	}
 
-	public int multiply(int num1, int num2) {
-		int result = num1 * num2;
+	public double multiply(double num1, double num2) {
+		double result = num1 * num2;
 		return result;
 	}
 
@@ -34,9 +34,9 @@ public class Calculator {
 		return result;
 	}
 
-	public double calculateAverage(int[] elements) {
+	public double calculateAverage(float[] elements) {
 		double result = elements[0];
-		for (int index = 0; index < elements.length; index++) {
+		for (int index = 1; index < elements.length; index++) {
 			result = result + elements[index];
 		}
 		double average = result / elements.length;
